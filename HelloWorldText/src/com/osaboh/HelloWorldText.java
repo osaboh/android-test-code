d79fccc224fc0788e87756c4d994ddb24ca9c6d8
Mikio Osanai <osaboh@gmail.com>
2009-02-22 22:16:33 +0900

    入力した

---
+++ HelloWorldText/src/com/osaboh/HelloWorldText.java
@@ -0,0 +1,13 @@
+package com.osaboh;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloWorldText extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
