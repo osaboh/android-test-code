cc8e43f198209d51e640034b4aafb9d9e73485a2
Mikio Osanai <osaboh@gmail.com>
2009-02-22 22:53:49 +0900

    ソースレベルで Activity を作成

--- HelloWorldText/src/com/osaboh/HelloWorldText.java
+++ HelloWorldText/src/com/osaboh/HelloWorldText.java
@@ -2,12 +2,20 @@ package com.osaboh;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HelloWorldText extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        // TextView のインスタンスを生成。インスタンスには Context オブジェクトを渡す。
+        // 以下の順で継承しているので HelloWorldText である this を渡す。
+        // Context <- ApplicationContext <- Activity <- HelloWorldText
+        TextView HelloWorldTextView = new TextView(this);
+        // 文字列を渡す。
+        HelloWorldTextView.setText("HelloWorld");
+        //　文字列を表示する。
+        setContentView(HelloWorldTextView);
     }
 }
